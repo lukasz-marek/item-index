34f83426b47bb67dbd2791025962acf131e97b5b
lmarek <marek.lucas@gmail.com>
2022-04-08 19:49:28 +0200

    add extra dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 val ktor_version: String by project
 val kotlin_version: String by project
+val koin_version: String by project
 val logback_version: String by project
-val prometeus_version : String by project
+val prometeus_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.6.20"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20"
 }
 
 group = "org.lmarek"
@@ -29,6 +30,12 @@ dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("org.postgresql:postgresql:42.3.3")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.strikt:strikt-core:0.34.0")
+    testImplementation("org.testcontainers:testcontainers:1.16.3")
 }
